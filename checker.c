351ab53bfefaa71017bc9ed50a132e51f2bf0421
PREM KUMAR R <80571286+premkumarar007@users.noreply.github.com>
2024-06-27 10:13:05 +0530

    Cyclomatic complexity reduced code.

--- checker.c
+++ checker.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 #include <assert.h>
 
-int isBatteryManagementSystemValuesWithinRange(float val,float min, float max,const char* message)
-{
- if(val<min || val>max)
- {
-   printf("%s\n",message);
-   return 0;
- }
-   return 1;
+int isBatteryManagementSystemValuesWithinRange(float value, float min, float max, const char* message) {
+  if (value < min || value > max) {
+    printf("%s\n", message);
+    return 0;
+  }
+  return 1;
 }
- 
+
 int batteryIsOk(float temperature, float soc, float chargeRate) {
- return isBatteryManagementSystemValuesWithinRange(temperature, 0,45, "Temperature out of range!") 
-  && isBatteryManagementSystemValuesWithinRange(soc, 20,80, "State of Charge out of range!") 
-  && isBatteryManagementSystemValuesWithinRange(chargeRate, 0, 0.8, "Charge Rate out of range!");
+  return isBatteryManagementSystemValuesWithinRange(temperature, 0, 45, "Temperature out of range!") &&
+         isBatteryManagementSystemValuesWithinRange(soc, 20, 80, "State of Charge out of range!") &&
+         isBatteryManagementSystemValuesWithinRange(chargeRate, 0, 0.8, "Charge Rate out of range!");
 }
 
-
- 
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
+  return 0;
 }
