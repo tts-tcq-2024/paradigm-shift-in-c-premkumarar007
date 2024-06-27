d0679575d7471044ab39b3ebea154cb776176a74
PREM KUMAR R <80571286+premkumarar007@users.noreply.github.com>
2024-06-27 11:25:29 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 
-int isWithinRange(float value, float min, float max, const char* message) {
+int isBatteryManagementSystemMeasuresWithinRange(float value, float min, float max, const char* message) {
   if (value < min || value > max) {
     printf("%s\n", message);
     return 0;
@@ -10,9 +10,9 @@ int isWithinRange(float value, float min, float max, const char* message) {
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  return isWithinRange(temperature, 0, 45, "Temperature out of range!")&&
-    isWithinRange(soc, 20, 80, "State of Charge out of range!")&&
-    isWithinRange(chargeRate, 0, 0.8, "Charge Rate out of range!");
+  return isBatteryManagementSystemMeasuresWithinRange(temperature, 0, 45, "Temperature out of range!")&&
+    isBatteryManagementSystemMeasuresWithinRange(soc, 20, 80, "State of Charge out of range!")&&
+    isBatteryManagementSystemMeasuresWithinRange(chargeRate, 0, 0.8, "Charge Rate out of range!");
 }
 
 int main() {
