e0b5ae36bcda4e9a7530a392fdec7047faf350cd
PREM KUMAR R <80571286+premkumarar007@users.noreply.github.com>
2024-07-29 10:53:28 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,21 +1,11 @@
-#include <stdio.h>
 #include <assert.h>
-
-int isBatteryManagementSystemMeasuresWithinRange(float value, float min, float max, const char* message) {
-  if (value < min || value > max) {
-    printf("%s\n", message);
-    return 0;
-  }
-  return 1;
-}
-
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  return isBatteryManagementSystemMeasuresWithinRange(temperature, 0, 45, "Temperature out of range!")&&
-    isBatteryManagementSystemMeasuresWithinRange(soc, 20, 80, "State of Charge out of range!")&&
-    isBatteryManagementSystemMeasuresWithinRange(chargeRate, 0, 0.8, "Charge Rate out of range!");
-}
+#include "battery_management.h"
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+    assert(batteryIsOk(25, 70, 0.7));
+    assert(!batteryIsOk(50, 85, 0));
+    assert(batteryIsOk(21, 75, 0.77));  // should print warnings
+    assert(!batteryIsOk(19, 81, 0.85)); // should print out of range messages
+
+    return 0;
 }
